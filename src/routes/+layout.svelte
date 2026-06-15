8d8b8190f418a0c8fb824af952d2b65902c8c693
Drew Johnson <werd2.718@gmail.com>
2026-06-14 23:59:04 -0600

    fix pwa for deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
     import '../style.css';
+    import { onMount } from 'svelte';
+    import { base } from '$app/paths';
+    import { dev } from '$app/environment';
+
 	let { children } = $props();
+
+    onMount(() => {
+        if ('serviceWorker' in navigator && !dev) {
+            navigator.serviceWorker.register(`${base}/service-worker.js`)
+                .then(reg => console.log('Service Worker registered', reg))
+                .catch(err => console.error('Service Worker registration failed', err));
+        }
+    });
 </script>
 
 <svelte:head>
